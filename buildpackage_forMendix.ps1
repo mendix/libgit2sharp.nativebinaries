4a75e48f25944b1bb8f9170e88dee656283556e3
Marcell van Rooyen <marcell.van.rooyen@mendix.com>
2020-09-16 10:50:20 +0200

    Change buildpackage_forMendix script to reflect Cloud build environment.

--- buildpackage_forMendix.ps1
+++ buildpackage_forMendix.ps1
@@ -3,7 +3,6 @@ Param(
     [string]$version
 )
 
-Remove-Item .\mx_nuget.package\runtimes\* -Recurse -Force
 Copy-Item .\nuget.package\runtimes\* .\mx_nuget.package\runtimes\ -Recurse -Force
 
 .\nuget.exe Pack mx_nuget.package\MendixBinaries.nuspec -Version $version -NoPackageAnalysis
