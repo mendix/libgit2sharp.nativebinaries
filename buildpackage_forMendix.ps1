5b08c5dd23458a72b0ab5cba8b24101ae443c8db
Marcell van Rooyen <marcell.van.rooyen@mendix.com>
2020-09-16 12:36:58 +0200

    Fix issue where buildpackage_forMendix.ps1 would not work on build agent

--- buildpackage_forMendix.ps1
+++ buildpackage_forMendix.ps1
@@ -4,6 +4,7 @@ Param(
 )
 
 Remove-Item .\mx_nuget.package\runtimes\* -Recurse -Force -ErrorAction SilentlyContinue
+mkdir .\mx_nuget.package\runtimes -Force
 Copy-Item -Path .\nuget.package\runtimes\* .\mx_nuget.package\runtimes\ -Recurse -Force
 
 .\nuget.exe Pack mx_nuget.package\MendixBinaries.nuspec -Version $version -NoPackageAnalysis
