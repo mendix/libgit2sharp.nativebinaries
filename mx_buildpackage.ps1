4c2c4da1c04b99c353fbd91cfaaa7c4fa18d1fe3
Marcell van Rooyen <marcell.van.rooyen@mendix.com>
2020-09-25 16:12:17 +0200

    Temp commit

--- mx_buildpackage.ps1
+++ mx_buildpackage.ps1
@@ -5,15 +5,15 @@ Param(
 
 # we remove any contents of the runtimes folder we also fail silently so that if the folder does not exist it does not return a error.
 # this will essentially force the folder to be deleted silently and recursively
-Remove-Item .\runtimes\* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item .\mx_nuget.package\runtimes\* -Recurse -Force -ErrorAction SilentlyContinue
 
 # Make the runtimes folder if it doesnt exist
-mkdir .\runtimes -Force
+mkdir .\mx_nuget.package\runtimes -Force
 
 #copy the contents of the nuget.packages runtimes folder to the mx_nuget.package runtimes folder.
 #this data is being populated by the build.libgit2.ps1 into the nuget.package folder
-Copy-Item -Path ..\nuget.package\runtimes\* .\runtimes\ -Recurse -Force
+Copy-Item -Path .\nuget.package\runtimes\* .\mx_nuget.package\runtimes\ -Recurse -Force
 
 #pack the package
-.\nuget.exe Pack .\MendixBinaries.nuspec -Version $version -NoPackageAnalysis
+.\nuget.exe Pack .\mx_nuget.package\MendixBinaries.nuspec -Version $version -NoPackageAnalysis
 
